2dce9c64450faae1c27fa6e050c2c82562da771a
Krzysztof <krzubrycki@gmail.com>
2022-01-25 19:11:20 +0100

    Added two publishers

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -22,8 +22,12 @@ public class Publisher {
     public Publisher() {
     }
 
-    public Publisher(String name) {
+    public Publisher(String name, String addressLine1, String city, String state, String zip) {
         this.name = name;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
     }
 
     public Long getId() {
